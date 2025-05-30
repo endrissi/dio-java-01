e7a8ca3587c88d76b3a13c089b2a4cb6b786748c
Lucas Endrissi <lucas_endrissi@outlook.com>
2025-05-29 23:08:55 -0300

    implementada regras na Classe ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("por favor, digite o número da conta:");
+        numero = sc.nextInt();
+        sc.nextLine(); //adicionado para evitar problemas na captura da entrada dos dados
+
+        System.out.println("por favor, digite a agencia:");
+        agencia = sc.nextLine();
+
+        System.out.println("por favor, digite o nome do cliente:");
+        nomeCliente = sc.nextLine();
+
+        System.out.println("por favor, digite o saldo da conta");
+        saldo = sc.nextDouble();
+
+        
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque." );
+
+        sc.close();
     }
 }
